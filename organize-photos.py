c26ff41226a34e83bcdfddf93ae96bf9cda0f72c
greenfacts <17084237+greenfacts@users.noreply.github.com>
2025-02-23 15:44:26 -0800

    handle png, add logging, add datelastmodified

--- organize-photos.py
+++ organize-photos.py
@@ -22,15 +22,15 @@ def get_date_from_exif(path):
 
 
 def get_date_from_filename(filename):
-    try:
-        # Extract the date part from the filename
-        date_str = filename.split("_")[1]
-        # Convert the date string to a datetime object
-        return datetime.strptime(date_str, "%Y%m%d")
-    except Exception as e:
-        print(f"Error getting date from {filename}: {e}")
+    parts = filename.split("_")
+    if len(parts) > 1 and parts[1].isdigit() and len(parts[1]) == 8:
+        date_str = parts[1]
+        if datetime.strptime(date_str, "%Y%m%d"):
+            return datetime.strptime(date_str, "%Y%m%d")
     return None
 
+def get_date_from_file_datelastmodified(path):
+    return datetime.fromtimestamp(os.path.getmtime(path))
 
 def organize_files(src_folder, dest_folder):
     if not os.path.exists(dest_folder):
@@ -38,8 +38,9 @@ def organize_files(src_folder, dest_folder):
 
     for root, dirs, files in os.walk(src_folder):
         for file in files:
-            if file.lower().endswith((".jpg", ".jpeg", ".mp4")):
+            if file.lower().endswith((".jpg", ".jpeg", ".mp4", ".png")):
                 src_path = os.path.join(root, file)
+
                 date_taken = (
                     get_date_from_exif(src_path)
                     if file.lower().endswith((".jpg", ".jpeg"))
@@ -47,9 +48,18 @@ def organize_files(src_folder, dest_folder):
                 )
                 if not date_taken:
                     date_taken = get_date_from_filename(file)
+                    if date_taken:
+                        print("Using date from filename instead of EXIF data")
+                if not date_taken:
+                    date_taken = get_date_from_file_datelastmodified(src_path)
+                    if date_taken:
+                        print("Using date from file last modified instead of EXIF data")
+
                 date_folder = (
                     date_taken.strftime("%Y-%m-%d") if date_taken else "unknown_date"
                 )
+                if date_folder == "unknown_date":
+                    print(f"Putting {file} in folder unknown_date")
 
                 dest_path = os.path.join(dest_folder, date_folder)
                 if not os.path.exists(dest_path):
@@ -58,7 +68,7 @@ def organize_files(src_folder, dest_folder):
                 shutil.copy2(src_path, dest_path)
                 print(f"Copied {src_path} to {dest_path}")
             else:
-                print(f"Skipping {file} as it is not a JPEG or MP4 file")
+                print(f"Skipping {file} as it is not a JPEG or MP4 file****************************************************************")
 
 
 # Check if source and destination folder paths are provided as arguments
